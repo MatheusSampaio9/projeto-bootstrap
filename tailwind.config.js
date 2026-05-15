595fdabe0cb84caa5f6d2bd834b3931cada4f4bb
MatheusSampaio9 <mazeupes2018@gmail.com>
2026-05-15 20:29:41 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+
+    content: ["./index.html"],
+  
+    theme: {
+      extend: {
+  
+        colors: {
+          customBlue: '#1e40af',
+        },
+  
+      },
+    },
+  
+    plugins: [],
+  
+  }
